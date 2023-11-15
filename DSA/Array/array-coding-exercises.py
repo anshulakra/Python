e1e1ce2030deffa41e19212b443ca9d5d03a657e
Anshu Lakra <anshulakra890@gmail.com>
2023-11-15 19:54:32 +0530

    new folder

---
+++ DSA/Array/array-coding-exercises.py
@@ -0,0 +1,20 @@
+# ### Missing number in Array
+
+# def missingNumber(arr,n):
+#     # finding sum of first n natural number
+#     sumNno = n * (n + 1) // 2
+
+#     # finding total sum in array
+#     sumArr = sum(arr)
+
+#     # finding missing number by subtracting sum Array from sum natural number (total)
+#     answer = sumNno - sumArr
+
+#     return answer
+
+# print(missingNumber([1,2,3,4,6],6))
+
+
+
+
+### 
